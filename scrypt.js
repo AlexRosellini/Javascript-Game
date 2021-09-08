3c78158ef6515efc1f99b42e93794ca15c89f0c2
taulmav <haloodstboss@live.fr>
2021-09-08 18:07:52 +0200

    initial commit

---
+++ scrypt.js
@@ -0,0 +1,23 @@
+const character = document.querySelector(".umu");
+const block = document.querySelector(".block")
+var counter=0;
+
+
+document.addEventListener("click", (event) => { 
+if (character.classList != "animate") {
+    character.classList.add("animate");
+}
+setTimeout(function () {
+    character.classList.remove("animate");
+},500)
+})
+
+let checkDead = setInterval(() => {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft<60 && blockLeft>-60 && characterTop>=120) {
+        block.style.animation = "none";
+        block.style.animation = "none";
+        alert("Game Over.");
+    }
+})
