8a94d708dcc28a0f68fc97fc23dec60ee9f4292b
taulmav <haloodstboss@live.fr>
2021-09-08 18:51:52 +0200

    score/Audio/Background

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,12 @@
 const character = document.querySelector(".umu");
 const block = document.querySelector(".block")
-var counter=0;
+let counter=0;
+let game = document.querySelector(".game");
+let button = document.querySelector(".button")
 
+button.addEventListener("click", (event) => {
+    window.location.reload();
+})
 
 document.addEventListener("click", (event) => { 
 if (character.classList != "animate") {
@@ -14,12 +19,18 @@ let audio = new Audio("umu.mp3")
 audio.play();
 })
 
+
+
 let checkDead = setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if (blockLeft<60 && blockLeft>-60 && characterTop>=120) {
         block.style.animation = "none";
         block.style.animation = "none";
-        alert("Game Over.");
+        alert("Congrats, "+Math.floor(counter/100) + " presents were delivered");
+        counter=0;
+    } else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
     }
 })
